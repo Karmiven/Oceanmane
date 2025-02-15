32e75fbeaac46a9ad2c501b81735ec0d56d9edf5
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-02-14 23:41:51 -0500

    Update NPC.sql

--- SQL/NPC.sql
+++ SQL/NPC.sql
@@ -299,7 +299,7 @@ INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, 
 
 
 -- *************************************************
--- *  Othiri Tideflame (Sword & Dagger Merchant)   *
+-- *  Othiri Tideflame (Cooking Trainer)           *
 -- *  (601011)                                     *
 -- *************************************************
 DELETE FROM `creature_template` WHERE (`entry` = 601011);
@@ -418,12 +418,12 @@ INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, 
 
 
 -- *************************************
--- *  Rohka Fartrader (Auctioneers)    *
+-- *  Rohka Fartrader (Auctioneer)    *
 -- *  (601015)                         *
 -- *************************************
 DELETE FROM `creature_template` WHERE (`entry` = 601015);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(601015, 0, 0, 0, 0, 0, 'Rohka Fartrader', NULL, NULL, 0, 50, 50, 0, 35, 2097152, 1, 1.14286, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 4096, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1.25, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
+(601015, 0, 0, 0, 0, 0, 'Rohka Fartrader', 'Auctioneer', NULL, 0, 50, 50, 0, 35, 2097152, 1, 1.14286, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 4096, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1.25, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
 
 DELETE FROM `creature_template_model` WHERE (`CreatureID` = 601015);
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
