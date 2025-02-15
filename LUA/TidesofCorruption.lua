6fc0c8f8888da3ece7ec6be2a326357227af52e4
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-02-14 19:55:45 -0500

    Update TidesofCorruption.lua

--- LUA/TidesofCorruption.lua
+++ LUA/TidesofCorruption.lua
@@ -4,17 +4,13 @@ local VAEROSTRAZ_TIDEKEEPER_MODEL_ID = 25835 -- Model ID for Vaerostraz the Tide
 local DESPAWN_TIME = 3000 -- 3 seconds in milliseconds
 
 local function OnZharkalisDeath(event, killer, zharkalis)
-    -- Find Corrupted Vaerostraz nearby
     local vaerostraz = zharkalis:GetNearestCreature(100, VAEROSTRAZ_CORRUPTED_ID)
     
     if vaerostraz then
-        -- Change model to Vaerostraz the Tidekeeper
         vaerostraz:SetDisplayId(VAEROSTRAZ_TIDEKEEPER_MODEL_ID)
         
-        -- Make him yell
         vaerostraz:SendUnitYell("The corruption... fades... I am free! Thank you, champions of Oceanmane!", 0)
 
-        -- Despawn him after 3 seconds
         vaerostraz:DespawnOrUnsummon(DESPAWN_TIME)
     end
 end
