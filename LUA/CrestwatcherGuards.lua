6a2278a5533a9f8d37ae18c4548c6b5c281860bd
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-02-15 15:12:46 -0500

    Create CrestwatcherGuards.lua

---
+++ LUA/CrestwatcherGuards.lua
@@ -0,0 +1,23 @@
+--Made by Manmadedrummer for Araxia Server
+
+local NPC_ID = 601016  -- NPC ID set to 601016
+
+local LOCATIONS = {
+    {name = "Caves", map = 1, x = -10724.191, y = 2189.4995, z = 4.0912714}
+}
+
+local CLASS_TRAINERS = {
+    {name = "Warrior Trainer", map = 1, x = -10754.1, y = 2415.43, z = 5.60095}
+}
+
+local PROFESSION_TRAINERS = {
+    {name = "Herbalism Trainer", map = 1, x = -10713.1, y = 2486.85, z = 7.92167},
+    {name = "Cooking Trainer", map = 1, x = 10764.6, y = 2423.47, z = 5.17545},
+    {name = "Blacksmith Trainer", map = 1, x = -10714.4, y = 2419.36, z = 7.60694}
+}
+
+local OTHER = {
+    {name = "Banker", map = 1, x = -10741.6, y = 2404.28, z = 6.96198},
+    {name = "Auctioneer", map = 1, x = -10722.8, y = 2434.07, z = 7.60413},
+    {name = "Mailbox", map = 1, x = 10725, y = 2471.15, z = 7.58996}
+}
