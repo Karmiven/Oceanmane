4c4c326df9fa85c27e91172e411e73cc1c2671e9
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-03-08 17:46:48 -0500

    Update NPC.sql

--- SQL/NPC.sql
+++ SQL/NPC.sql
@@ -353,21 +353,21 @@ INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, 
 -- *  Tamuun Saltbinder (Banker)       *
 -- *  (601013)                         *
 -- *************************************
-DELETE FROM `creature_template` WHERE (`entry` = 8356);
+DELETE FROM `creature_template` WHERE (`entry` = 601013);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(8356, 0, 0, 0, 0, 0, 'Tamuun Saltbinder', 'Banker', NULL, 0, 45, 45, 0, 35, 131072, 1, 1.14286, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 4608, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 3, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
+(601013, 0, 0, 0, 0, 0, 'Tamuun Saltbinder', 'Banker', NULL, 0, 45, 45, 0, 35, 131072, 1, 1.14286, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 4608, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 3, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
 
-DELETE FROM `creature_template_model` WHERE (`CreatureID` = 8356);
+DELETE FROM `creature_template_model` WHERE (`CreatureID` = 601013);
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
-(8356, 0, 7621, 1, 1, 12340);
+(601013, 0, 7621, 1, 1, 12340);
 
-DELETE FROM `creature_equip_template` WHERE (`CreatureID` = 8356);
+DELETE FROM `creature_equip_template` WHERE (`CreatureID` = 601013);
 INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`, `VerifiedBuild`) VALUES
-(8356, 1, 12744, 0, 0, 18019);
+(601013, 1, 12744, 0, 0, 18019);
 
-DELETE FROM `creature` WHERE (`id1` = 8356);
+DELETE FROM `creature` WHERE (`id1` = 601013);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
-(9001784, 8356, 0, 0, 1, 0, 0, 1, 1, 1, -10741.6, 2404.28, 6.96198, 1.55453, 300, 0, 0, 6930, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
+(9001784, 601013, 0, 0, 1, 0, 0, 1, 1, 1, -10741.6, 2404.28, 6.96198, 1.55453, 300, 0, 0, 6930, 0, 0, 0, 0, 0, '', NULL, 0, NULL);
 
 
 -- *************************************
